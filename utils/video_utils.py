a2e2ece28b75ad9d471a289997a26676d0e1742e
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-03-08 00:45:42 +0200

    Changed function docstring format

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -4,8 +4,12 @@ import cv2
 def get_duration(video: cv2.VideoCapture) -> float:
     """
     Get Video Duration in Seconds
-    :param video: OpenCV VideoCapture
-    :return: Video duration in seconds
+
+    Args:
+        video: OpenCV VideoCapture
+
+    Returns:
+        Video duration in seconds
     """
     frames_per_second = video.get(cv2.CAP_PROP_FPS)
     frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
