adbbcceea940c756653271e6f16094746c09ab6b
naiera-magdy <44544070+naiera-magdy@users.noreply.github.com>
2022-03-20 14:06:11 +0200

    Extraction of facial landmarks
    Trained facial landmark features on CK+ dataset

--- FacialExpressionRecognition/Classify.py
+++ FacialExpressionRecognition/Classify.py
@@ -5,24 +5,42 @@ from sklearn.svm import SVC
 import FeaturesExtraction
 import Utils
 import pickle as pickle
-
+import DatasetLoading
 # Read Image and Model
-path_to_file = os.path.join(os.path.dirname(
-    __file__), "../Test/disgust.jpg")
+# path_to_file = os.path.join(os.path.dirname(
+#     __file__), "../Test/disgust2.jpg")
+# img = cv2.imread(path_to_file, cv2.IMREAD_GRAYSCALE)
 
-img = cv2.imread(path_to_file, cv2.IMREAD_GRAYSCALE)
+# featureExtractor = FeaturesExtraction.FeatureExtractor(True)
 model: SVC = pickle.load(open('Model.sav', 'rb'))
-featureExtractor = FeaturesExtraction.FeatureExtractor(True)
+
+start_time = time.time()
+
+# Load data with extracted features
+print('Loading data and extract features. This will take time ...')
+features, labels = DatasetLoading.load_test_data("../Test/TFEID")
+print('Finished loading data.')
 
 # Calculate time before processing
-start_time = time.time()
-img_features = featureExtractor.ExtractHOGFeatures(img)
-img_features = featureExtractor.ApplyPCAonFeatures(img_features.reshape(1,-1))[0]
-predicted = model.predict([img_features])[0]
+# img_features = featureExtractor.ExtractHOGFeatures(img)
+# img_features = featureExtractor.ApplyPCAonFeatures(img_features.reshape(1,-1))[0]
+# img_features = featureExtractor.ExtractLandMarks(img)
+# img_features = featureExtractor.normalizeFeatures(img_features)
+
+# predicted = model.predict([img_features])[0]
+# accuracy = model.score(features, labels)
+
+predicted = model.predict(features)
+accuracy = sum(predicted == labels) / len(labels)
 
 # Calculate time after processing in seconds
 end_time = time.time()
 print("[INFO] Time taken is {:.5f} seconds".format(end_time - start_time))
 
-print(predicted)
-Utils.show_image(img, predicted)
+print("Classification accuracy: ",accuracy*100, "%")
+# Utils.show_image(img, predicted)
+
+f = open("out.txt", "w")
+for i in range(len(labels)):
+    f.write(f"{predicted[i]}  {labels[i]}\n")
+f.close()
