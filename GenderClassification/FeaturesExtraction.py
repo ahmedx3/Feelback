fbc2f329e069716a9c99cc06efb9a962def62ace
Happysword <45149700+Happysword@users.noreply.github.com>
2022-03-24 15:47:29 +0200

    Gender Classification: K fold validation, Realtime gender detection with webcam

--- GenderClassification/FeaturesExtraction.py
+++ GenderClassification/FeaturesExtraction.py
@@ -13,37 +13,41 @@ def extract_LPQ(img, winSize=5):
         np.array: Histogram values of LPQ for Image
     """
 
-    img = np.float64(img)  # Convert np.image to double
-    radius = int(winSize / 2 - 0.5)  # Get radius from window size
+    # Convert np.image to double
+    img = np.float64(img)
+    # Calculate radius from window size
+    radius = int(winSize / 2 - 0.5)
     # Form spatial coordinates in window
-    x = np.arange(-radius, radius+1)[np.newaxis]
+    filters_shape = np.arange(-radius, radius+1)[np.newaxis]
 
-    w0 = np.ones_like(x)
-    w1 = np.exp(-2*np.pi*x * (1/winSize)*1j)
-    w2 = np.conj(w1)
+    # Create Filters
+    filter_one = np.ones_like(filters_shape)
+    filter_two = np.exp(-2*np.pi*filters_shape * (1/winSize)*1j)
+    filter_three = np.conj(filter_two)
 
-    # Run filters to compute the frequency response in the four points. Store np.real and np.imaginary parts separately
-    # Run first filter
-    fr_1 = convolve2d(convolve2d(img, w0.T, 'valid'), w1, 'valid')
-    fr_2 = convolve2d(convolve2d(img, w1.T, 'valid'), w0, 'valid')
-    fr_3 = convolve2d(convolve2d(img, w1.T, 'valid'), w1, 'valid')
-    fr_4 = convolve2d(convolve2d(img, w1.T, 'valid'), w2, 'valid')
+    # Run filters to compute the frequency response in the four points
+    temp_img_filter_two = convolve2d(img, filter_two.T, 'valid')
+    temp_img_filter_one = convolve2d(img, filter_one.T, 'valid')
+    fourier_response_one = convolve2d(temp_img_filter_one, filter_two, 'valid')
+    fourier_response_two = convolve2d(temp_img_filter_two, filter_one, 'valid')
+    fourier_response_three = convolve2d(temp_img_filter_two, filter_two, 'valid')
+    fourier_response_four = convolve2d(temp_img_filter_two, filter_three, 'valid')
 
-    # Initilize frequency domain matrix for four frequency coordinates (np.real and np.imaginary parts for each frequency).
-    freqResp = np.dstack([fr_1.real, fr_1.imag,
-                          fr_2.real, fr_2.imag,
-                          fr_3.real, fr_3.imag,
-                          fr_4.real, fr_4.imag])
+    # Initilize frequency domain matrix for four frequency coordinates.
+    stacked_fourier_response = np.dstack([fourier_response_one.real, fourier_response_one.imag,
+                          fourier_response_two.real, fourier_response_two.imag,
+                          fourier_response_three.real, fourier_response_three.imag,
+                          fourier_response_four.real, fourier_response_four.imag])
 
     # Perform quantization and compute LPQ codewords
-    inds = np.arange(freqResp.shape[2])[np.newaxis, np.newaxis, :]
-    LPQdesc = ((freqResp > 0)*(2**inds)).sum(2)
+    inds = np.arange(stacked_fourier_response.shape[2])[np.newaxis, np.newaxis, :]
+    calculated_LPQ = ((stacked_fourier_response > 0)*(2**inds)).sum(2)
 
     # Normalize histogram
-    LPQdesc = np.histogram(LPQdesc.flatten(), range(256))[0]
-    LPQdesc = LPQdesc/LPQdesc.sum()
+    calculated_LPQ = np.histogram(calculated_LPQ.flatten(), range(256))[0]
+    calculated_LPQ = calculated_LPQ/calculated_LPQ.sum()
 
-    return LPQdesc
+    return calculated_LPQ
 
 def extract_LBP(img, radius=3, eps=1e-7):
     """Extract LBP
@@ -58,7 +62,7 @@ def extract_LBP(img, radius=3, eps=1e-7):
     """
 
     n_points = 8 * radius
-    lbp = local_binary_pattern(img, n_points, radius, 'default')
+    lbp = local_binary_pattern(img, n_points, radius, 'uniform')
     (hist, _) = np.histogram(lbp.ravel(), bins=256)
     hist = hist.astype("float")
     hist /= (hist.sum() + eps)
