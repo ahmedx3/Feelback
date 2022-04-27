e704d6d0940c76a0e7467bdbe73c0eab62b25e4d
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-04-27 14:10:34 +0200

    Integrated Face Tracker into Pipeline

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from utils import verbose
 from utils import io
 from utils import video_utils
 from FaceDetection.HOG_SVM.main import FaceDetector
+from FaceTracking.knn import KNNIdentification
+
+import numpy as np
+
 
 def main():
     args = io.get_command_line_args()
@@ -25,7 +29,13 @@ def main():
     ########################### Initialize FaceDetection ###########################
     modelPath = './FaceDetection/HOG_SVM/Models/Model_v3.sav'
     pcaPath = './FaceDetection/HOG_SVM/Models/PCA_v3.sav'
-    faceDetector = FaceDetector(modelPath,pcaPath)
+    faceDetector = FaceDetector(modelPath, pcaPath)
+
+    ########################### Initialize FaceTracking ###########################
+    faceTracker = KNNIdentification()
+
+    # assign some unique colors for each face id for visualization purposes
+    colors = [(0, 0, 255), (255, 0, 0), (0, 255, 0), (255, 0, 255), (255, 255, 0), (128, 255, 0), (255, 128, 0)] * 10
 
     frame_number = 0
     # Read frame by frame until video is completed
@@ -36,16 +46,27 @@ def main():
 
         verbose.print(f"[INFO] Processing Frame #{frame_number}")
 
+        frame_number += int(video_fps / frames_to_process_each_second)  # Process N every second
+
+        # Seek the video to the required frame
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+
         # ============================================= Preprocessing =============================================
+        frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # ============================================= Face Detection ============================================
         faces = faceDetector.detect(frame)
-        verbose.print(f"[INFO] Detected {len(faces)} faces")
-        for (x, y, lenX,lenY) in faces:
-            cv2.rectangle(frame, (int(x), int(y)), (int(lenX), int(lenY)), (0, 255, 0), 2)
+        faces = np.array(faces, dtype=int)
+        verbose.print(f"[INFO] Detected {faces.shape[0]} faces")
+
+        if faces is None or len(faces) == 0:
+            verbose.print("No Faces Detected, Skipping this frame")
+            continue
+
         # ========================================= Emotion Classification ========================================
 
         # ============================================= Face Tracking =============================================
+        ids = faceTracker.get_ids(frame_grey, faces)
 
         # ============================================ Gaze Estimation ============================================
 
@@ -55,16 +76,20 @@ def main():
 
         # =============================================== Analytics ===============================================
 
-        # =========================================================================================================
-        
+        # ========================================== Verbosity Printing ===========================================
+
+        if verbose.__VERBOSE__:
+            # Draw a rectangle around each face with its person id
+            for i in range(len(ids)):
+                x1, y1, x2, y2 = faces[i]
+                cv2.rectangle(frame, (x1, y1), (x2, y2), color=colors[ids[i]], thickness=2)
+                cv2.putText(frame, f"Person #{ids[i]}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[ids[i]], 2)
+
         verbose.imshow(frame, delay=1)
 
-        frame_number += int(video_fps / frames_to_process_each_second)  # Process N every second
-
-        # Seek the video to the required frame
-        video.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-
         verbose.print(f"[INFO] Video Current Time is {round(video.get(cv2.CAP_PROP_POS_MSEC), 3)} sec")
+
+        # =========================================================================================================
 
     # When everything done, release the video capture object
     video.release()
