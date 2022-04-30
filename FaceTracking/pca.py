47b4314a09df52f53025d10767b079415e1df479
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-04-30 05:03:58 +0200

    Optimized Memory usage in PCA
    Now uses less than half memory

--- FaceTracking/pca.py
+++ FaceTracking/pca.py
@@ -37,10 +37,16 @@ class PCA:
         # Center data around origin by convert it to zero mean
         z = x - self.mean
 
-        covariance_matrix = np.cov(z, rowvar=False)  # cov = (z.T @ z) / z.shape[0]
-        eigen_values, eigen_vectors = np.linalg.eigh(covariance_matrix)
+        # use this formula instead of `np.cov(z.T)` because it uses much less memory (about half)
+        covariance_matrix = (z.T @ z) / z.shape[0]
+        del z
 
+        eigen_values, eigen_vectors = np.linalg.eigh(covariance_matrix)
+        del covariance_matrix
+        
         largest_n_eigen_values_indices = eigen_values.argsort()[-self.n:][::-1]
+        del eigen_values
+
         self.principle_components = eigen_vectors[:, largest_n_eigen_values_indices].T
 
     def transform(self, x: np.ndarray) -> np.ndarray:
