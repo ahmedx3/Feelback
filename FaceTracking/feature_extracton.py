16ae464e16bcb352ed42230b3e2ab7de77d4ac6a
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-05-01 16:19:46 +0200

    Optimized FaceTracking Speed

--- FaceTracking/feature_extracton.py
+++ FaceTracking/feature_extracton.py
@@ -7,6 +7,8 @@ from typing import List
 __CURRENT_DIR__ = os.path.dirname(os.path.abspath(__file__))
 __PARENT_DIR__ = os.path.dirname(__CURRENT_DIR__)
 
+pca = pickle.load(open(f"{__CURRENT_DIR__}/models/pca_n=50_affectnet.sav", 'rb'))
+
 
 def eigen_faces_features(faces: List[np.ndarray]) -> np.ndarray:
     face_size = (48, 48)
@@ -15,6 +17,5 @@ def eigen_faces_features(faces: List[np.ndarray]) -> np.ndarray:
         # Resize face to a fixed size then flatten it
         resized_faces[i] = cv2.resize(face, face_size).ravel()
 
-    pca = pickle.load(open(f"{__CURRENT_DIR__}/models/pca_n=50_affectnet.sav", 'rb'))
     eigen_faces = pca.transform(resized_faces)
     return eigen_faces
