9637bdfc79dad55ecd5dbccc58c054929b9f0c26
ahmedmostafa-incorta <ahmed.moustafaa99@gmail.com>
2022-05-17 23:56:31 +0200

    Added trailer and reaction videos to dashboard page

--- feelback-front/src/main.js
+++ feelback-front/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
 import store from './store';
+import router from './router';
 import vuetify from './plugins/vuetify';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
--- feelback-front/src/router/index.js
+++ feelback-front/src/router/index.js
@@ -10,14 +10,14 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue'),
+  },
 ];
 
 const router = new VueRouter({
