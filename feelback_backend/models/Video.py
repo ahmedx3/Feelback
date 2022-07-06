201bd2a7d1f5993c8945d0ee4e132ab0c39657a1
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-07-06 16:11:27 +0200

    [Backend] Made Trailer/Video Relation One-to-One

--- feelback_backend/models/Video.py
+++ feelback_backend/models/Video.py
@@ -25,7 +25,7 @@ class Video(BaseModel):
     finished_processing = db.Column(db.Boolean, nullable=False, default=False)
     progress = db.Column(db.Float, nullable=True, default=0.0)
 
-    trailer = db.relationship('Video', backref='reaction', remote_side=[id], lazy=True)
+    trailer = db.relationship('Video', backref=db.backref('reaction', uselist=False), remote_side=[id], lazy=True)
     persons = db.relationship('Person', backref='video', lazy=True)
 
     def __init__(self, id: str, filename: str, frame_count: int, duration: float, progress: float = 0.0,
@@ -41,7 +41,7 @@ class Video(BaseModel):
                 base_url: str = None):
         json = BaseModel.to_json(self, populate, exclude_columns, include_foreign_keys)
         json['trailer_id'] = self.trailer_id
-        json['reaction_id'] = self.reaction[0].id if self.reaction and len(self.reaction) > 0 else None
+        json['reaction_id'] = self.reaction.id if self.reaction is not None else None
 
         if base_url is not None:
             json["img_url"] = f"{base_url}/{self.id}/thumbnail"
