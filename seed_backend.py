b8aca4d6ad40120275893d787037f85a1ab6e536
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-07-14 19:01:56 +0200

    [Backend] Allow Seeding With a Trailer Video

--- seed_backend.py
+++ seed_backend.py
@@ -15,16 +15,27 @@ def get_args():
     parser = argparse.ArgumentParser(description='Load feelback data into the database without processing the video.')
     parser.add_argument('video_filename', type=str, help='Path to the video file')
     parser.add_argument('--load-feelback', '-l', type=str, required=True, help='Path to feelback dumped pickle file')
+    parser.add_argument('--trailer', '-t', type=str, required=False, help='Path to Trailer Video File to Add')
     return parser.parse_args()
 
 
-def main(video_filename, feelback):
+def main(video_filename, trailer, feelback):
     app.testing = True
     client = app.test_client()
+
+    trailer_id = None
+    if trailer:
+        response = client.post('/api/v1/videos', data={
+            'video': open(trailer, 'rb'),
+            'type': 'Trailer',
+        })
+        assert response.status_code == Status.CREATED
+        trailer_id = response.json['data']['id']
 
     response = client.post('/api/v1/videos', data={
         'video': open(video_filename, 'rb'),
         'type': 'Reaction',
+        'trailer_id': trailer_id,
     })
 
     assert response.status_code == Status.CREATED
@@ -36,6 +47,6 @@ def main(video_filename, feelback):
 if __name__ == '__main__':
     args = get_args()
     feelback = pickle.load(open(args.load_feelback, 'rb'))
-    main(args.video_filename, feelback)
+    main(args.video_filename, args.trailer, feelback)
 
 
