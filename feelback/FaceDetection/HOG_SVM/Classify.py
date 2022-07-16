c29084075a5130a7d557d64eefca75cf412f7aa0
ahmedmostafa-incorta <ahmed.moustafaa99@gmail.com>
2022-07-16 13:57:05 +0200

    Update Classify.py

--- feelback/FaceDetection/HOG_SVM/Classify.py
+++ feelback/FaceDetection/HOG_SVM/Classify.py
@@ -19,23 +19,23 @@ import time
 
 ################################## Hyperparameters ##################################
 (winW, winH) = (19, 19) # window width and height
-pyramidScale = 2 # Scale factor for the pyramid
+pyramidScale = 1.5 # Scale factor for the pyramid
 stepSize = 2 # Step size for the sliding window
 overlappingThreshold = 0.3 # Overlap threshold for non-maximum suppression
 skinThreshold = 0.4 # threshold for skin color in the window
 edgeThreshold = 0.2 # threshold for edge percentage in the window
 #####################################################################################
 
-originalImg = cv2.imread("../HOG_SVM/Examples/Test18.jpg")
+originalImg = cv2.imread("../HOG_SVM/Examples/Our_Faces.jpg")
 
 print("[INFO] Shape of the original image ", originalImg.shape)
 shapeBefore = originalImg.shape
 copyOriginalImage = originalImg.copy()
 
-originalImg = cv2.resize(originalImg, (int(originalImg.shape[1]/2), int(originalImg.shape[0]/2)))
+originalImg = cv2.resize(originalImg, (int(originalImg.shape[1]/8), int(originalImg.shape[0]/8)))
 print("[INFO] Shape of the image after reshaping", originalImg.shape)
 
-modelName = "./Models/Model_v3.sav"
+modelName = "./Models/Model_v4.sav"
 model = pickle.load(open(modelName, 'rb'))
 faces = []
 
@@ -56,18 +56,24 @@ for image in pyramid(originalImg, pyramidScale, minSize=(30, 30)):
         break
     print("[INFO] Num of windows in the current image pyramid ",len(windows))
 
+    # indices, patches = zip(*windows)
+    # patches_hog = np.array([ ApplyPCA(ExtractHOGFeatures(patch,flatten=True),pca) for patch in patches])
+    # predicted_label = model.predict(patches_hog)
+    # indices = np.array(indices)
+
     indices, patches = zip(*windows)
     st1 = time.time()
     grayScaledPatches = [HistogramEqualization(patch) for patch in patches]
     hogFeatures = vectorizedHogSlidingWindows(grayScaledPatches)
     patches_hog = []
     for i in range(len(windows)):
-        patches_hog.append(ApplyPCA(hogFeatures[:,:,:,:,:,i].flatten(),pca))
-
+        patches_hog.append(hogFeatures[:,:,:,:,:,i].flatten())
     st2 = time.time()
-    print("[INFO] HOG a7a taken is {:.5f} seconds".format(st2 - st1))
 
+    pt1 = time.time()
     predicted_label = model.predict(patches_hog)
+    pt2 = time.time()
+    print("[INFO] Prediciton time is {:.5f} seconds".format(pt2 - pt1))
     indices = np.array(indices)
     for i, j in indices[predicted_label == "Faces"]:
         faces.append((int(i * scaleFactor), int(j * scaleFactor), int((i + winW) * scaleFactor), int((j + winH) * scaleFactor)))
@@ -79,7 +85,7 @@ end_time = time.time()
 print("[INFO] Time taken is {:.5f} seconds".format(end_time - start_time))
 
 for (x, y, lenX,lenY) in faces:
-    cv2.rectangle(copyOriginalImage, (int(x), int(y)), (int(lenX), int(lenY)), (0, 255, 0), 2)
+    cv2.rectangle(copyOriginalImage, (int(x), int(y)), (int(lenX), int(lenY)), (0, 255, 0), 6)
 
 # Save the image result
 cv2.imwrite("result.jpg", copyOriginalImage)
